4e579a990944462ade1b169aaa0ed2478875c343
syoutamaeda <maeda1995@docomo.ne.jp>
2021-07-04 10:11:38 +0000

    rails05b

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { in: 1..15 }
-  PW_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
-  validates :password, presence: true, format: { with: PW_REGEX }
-  EM_REGEX = /[a-zA-Z0-9._+]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*/
-  validates :email, presence: true, format: { with: EM_REGEX }
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+
   has_secure_password
+
+  has_many :topics
 end
